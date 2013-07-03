7b083aa937eda62f31be75efe8d4003269df940d
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2013-07-03 16:36:27 +0200

    fix relative path and add .gitignore in Max's code

--- lables/load.py
+++ lables/load.py
@@ -9,20 +9,17 @@ from matplotlib import pyplot as plot
 
 if __name__ == "__main__":
     
-    f = h5py.File("block00.h5",'r')
+    f = h5py.File("../data/block00.h5",'r')
     
     data = f['volume']['data']
     
     for i in range(0,data.shape[2],10):
         vigra.impex.writeImage(data[:,:,i],"block00_"+str(i)+".png")
-        '''
+
         plot.imshow(data[:,:,i])
         plot.show()
         time.sleep(.2)
         plot.clf()
         plot.close()
-        '''
-    import pdb
-    pdb.set_trace()
 
 
