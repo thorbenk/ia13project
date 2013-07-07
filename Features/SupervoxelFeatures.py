8fe09c7d88087007c0688197c662153ac552fb83
Anne-Christine Scherzer <scherzer@stud.uni-heidelberg.de>
2013-07-07 13:22:33 +0200

    implemented supervoxel features size and PCA

--- Features/SupervoxelFeatures.py
+++ Features/SupervoxelFeatures.py
@@ -1,3 +1,6 @@
+import numpy as np
+from numpy.linalg import svd
+
 # Supervoxel Features take a whole supervoxel and try to extract information
 # from the shape.
 
@@ -37,21 +40,86 @@ class SupervoxelFeature:
 
 
 
-#class SizeFeature(SupervoxelFeature):
-#    
-#    def numFeatures(self):
-#        return 1
-#
-#    def features(self, supervoxel):
-#        return array([100])
+class SizeFeature(SupervoxelFeature):
     
+    def numFeatures(self):
+        return 1
+
+    def features(self, supervoxel):
+        return np.array([len(supervoxel)])
+
+
+# TODO: find principal component directions, too
+class PCA(SupervoxelFeature):
+
+    def __init__(self, noAxes=2, calcEigVectors=False):
+        self.axes = noAxes
+        self.calcEigVectors = calcEigVectors
+
+    def numFeatures(self):
+        if self.calcEigVectors:
+            return self.axes + 3*self.axes
+        else:
+            return self.axes
+
+    def features(self, supervoxel):
+        if self.axes > 3:
+            print 'WARNING: max number of principal axes can be 3. Will set noAxes = 3'
+            self.axes = len(supervoxel)
+
+        # calculate center of mass (cm)
+        cm_x = np.mean(supervoxel[:,0])
+        cm_y = np.mean(supervoxel[:,1])
+        cm_z = np.mean(supervoxel[:,2])
+        cm = np.array([cm_x, cm_y, cm_z])
+        # change origin to center of mass
+        supervoxel -= cm
+
+        # write coordiates into columns of a matrix
+        supervoxel = supervoxel.swapaxes(0, 1)
+        u, s, v = svd(supervoxel, full_matrices=True, compute_uv=True)
+
+        if self.calcEigVectors:
+            pass
+        else:
+            return np.array(s)
 
 ## TODO: PCA, Convex Hull Volume, Shape Probability, ...
 
-
-
 #test the channel generators here
 if __name__ == "__main__":
-    pass
 
+    supervoxel = np.array([[1, 2, 3], [4, 5, 6], [7, 7, 7], [5, 4, 8]])
+    
+    # test size
+    testSize = SizeFeature()
+    numFeatures = testSize.numFeatures()
+    size = testSize.features(supervoxel)
+    assert type(size) == np.ndarray, 'type of size is {0}, should be np.ndarray'.format(type(size))
+    print 'number of features:', numFeatures
+    print 'size:', size
+    ###
 
+    # test PCA
+    testPCA = PCA(noAxes=3, calcEigVectors=False)
+    numFeatures = testPCA.numFeatures()
+    singValues = testPCA.features(supervoxel)
+    assert type(size) == np.ndarray, 'type of size is {0}, should be np.ndarray'.format(type(size))
+    print 'number of features:', numFeatures
+    print 'singular values:', singValues
+
+    """
+    import matplotlib as mpl
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure("Test PCA")
+    ax = fig.gca(projection='3d')
+    x = supervoxel[:,0]
+    y = supervoxel[:,1]
+    z = supervoxel[:,2]
+
+    ax.plot(x, y, z, 'ko')
+    #plt.show()
+    """
+    ###
