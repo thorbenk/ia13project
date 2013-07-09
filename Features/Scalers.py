09a06392eb2e344f15a235cc9ab289033c21fb7b
Karsten Krispin <karsten@nonlimited.org>
2013-07-09 17:18:18 +0200

    implemented Gauss Smooth Scaler

--- Features/Scalers.py
+++ Features/Scalers.py
@@ -1,8 +1,13 @@
-# Scalers take raw data in scale them respectively. This way we can retrieve
-# information on different level of details
-#
-# We want to use dedicated scalers in order to speed things up. So we only need to
-# scale the image once and all channel generators can share one scaled version.
+import numpy as np
+from vigra.filters import *
+
+"""
+Scalers take raw data in scale them respectively. This way we can retrieve
+information on different level of details
+
+We want to use dedicated scalers in order to speed things up. So we only need to
+scale the image once and all channel generators can share one scaled version.
+"""
 
 class AbstractScaler:
     """ 
@@ -17,9 +22,11 @@ class AbstractScaler:
         """ this function returns the scaled version of a given 3d-image """
         raise NotImplementedError()
 
+    def __str__(self):
+        return "AbstractScaler"
 
 
-class DummyScaler(AbstractScaler):
+class TestScaler(AbstractScaler):
     """ Dummy Scaler, does nothing """
     def scaled(self, image):
         print "DummyScaler: ", image.shape
@@ -40,7 +47,35 @@ class GaussianScaler(AbstractScaler):
         self.sigma = sigma
 
     def scaled(self, image):
-        return AbstractScaler.scaled(self, image)
+        return gaussianSmoothing(image, self.sigma)
 
     def __str__(self):
         return "GScaler["+str(self.sigma)+"]"
+
+
+
+if __name__ == "__main__":
+
+    ## create test data
+    # size of the voxel
+    n = 11
+
+    # center of the impulse
+    c = 5
+    test = np.float32(np.zeros((n, n, n)))
+    test[c, c, c] = 1
+
+    ## TestScaler
+    tscaler = TestScaler()
+    tscalerTest = tscaler.scaled(test)
+    assert(tscalerTest.shape == test.shape)
+
+
+    ## GaussianScaler
+    # uses gaussian filter kernel
+    gscaler = GaussianScaler(1.0)
+    gscalerTest = gscaler.scaled(test)
+    assert(gscalerTest.shape == test.shape)
+
+    print test[c-1:c+2, c-1:c+2, c-1:c+2]
+    print gscalerTest[c-1:c+2, c-1:c+2, c-1:c+2]
