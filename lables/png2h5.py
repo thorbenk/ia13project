f4f4a06e2d22586e375fc67cf6d7aa21933b95c9
max argus <argus.max@gmail.com>
2013-07-10 18:37:41 +0200

    seems to work now

--- lables/png2h5.py
+++ lables/png2h5.py
@@ -2,8 +2,8 @@ from os import listdir, getcwd
 from os.path import isfile, join
 
 import numpy as np
-#import vigra
-#import h5py
+import vigra
+import h5py
 import time
 import os
 """
@@ -20,53 +20,67 @@ block00_<slice>_labels.png
 5 PSD             green      0 255   0
 """
 
-labels = np.array( ( (255, 255, 255),
-		     (0  ,   0, 255),
-	             (255,   0,   0),
-		     (255, 255,   0),
-		     (  0, 255,   0)))
+labels = np.array( 
+    ( (255, 255, 255,0),
+      (0  ,   0, 255,0),
+      (255,   0,   0,0),
+      (255, 255,   0,0),
+      (  0, 255,   0,0)))
 
 def getLabel(rgb):
-	res = labels  - rgb
-	
-	res = np.apply_along_axis(np.linalg.norm,1,res)
-	
-	return np.argmin( res )
+    
+    res = labels  - rgb
+    res = np.apply_along_axis(np.linalg.norm,1,res)
+
+    return np.argmin( res ) +1
 	
 
 def filename2addr(filename):
 	s = filename.split("_")
 	return s[:2]
 
+four_zeros = np.zeros(4)
+
 def file2array(path):
-	#im = vigra.impex.readImage(path)
-	img = np.zeros((255,255,4))
-	label_img = np.zeros(img.shape)
-	
-	for x in range(len(img.shape[0])):
-		for y in range(len(img.shape[1])):
-			label_img = getLabel(img[x][y])
-	return label_img
+    img = vigra.impex.readImage(path).view(np.ndarray)
+    #img = np.zeros((255,255,4))
+    label_img = np.zeros(img.shape[:2])
+    #print label_img.shape
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            cur_rgb = img[x][y]
+            if (cur_rgb != four_zeros).all():
+                label_img[x,y] = getLabel(img[x][y])
+            else:
+                label_img[x,y] = 0;
+
+    return label_img
 	
 if __name__ == "__main__":
-	mypath = getcwd()
-	files = [ f for f in listdir(mypath) if ( isfile(join(mypath,f)) and f.endswith("labels.png")) ]
-	
-	name = "block00"
-	filename = os.path.join("../","data",name+".h5")
-	filename_out = os.path.join("../","data",name+"_labels.h5")
+    mypath = getcwd()
+    files = [ f for f in listdir(mypath) if ( isfile(join(mypath,f)) and f.endswith("labels.png")) ]
+    
+    name = "block00"
+    filename = os.path.join("../","data",name+".h5")
+    filename_out = os.path.join("../","data",name+"_labels.h5")
 
-	#f = h5py.File("../block00.h5",'r')
-    	#data_shape = f['volume']['data'].shape
-	data_shape = (255,255,255)
+    f = h5py.File("../data/block00.h5",'r')
+    data_shape = f['volume']['data'].shape
+    #data_shape = (255,255,255)
 
-	label_ar = np.zeros(data_shape)
+    label_ar = np.zeros(data_shape)
+    #print label_ar.shape
 
-	for f in files:
-		h5file,slice = filename2addr(f)
-		label_ar[:,:,slice] = file2array(f)
-	
-	f2 = h5py.File(filename_out, 'w')
-    	f2.create_dataset("volume/data", data=label_ar.astype(numpy.uint8))
-    	f2.close()
+    for f in files:
+        print f
+        h5file,slice = filename2addr(f)
+        res =  file2array(f)
+        
+        #import pdb
+        #pdb.set_trace()
+        
+        label_ar[:,:,slice] =  res 
+    f2 = h5py.File(filename_out, 'w')
+    f2.create_dataset("volume/data", data=label_ar.astype(np.uint8))
+    f2.close()
 
