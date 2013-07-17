f0c73a385c8453bf2892a1312a30cde91e95ef8a
Karsten Krispin <krispin@stud.uni-heidelberg.de>
2013-07-17 11:34:17 +0200

    added channel histogram feature
    TODO: normalize the values generated by channel-feature to 0.0 ... 1.0 in order to have a meaningfull statistic

--- Features/ChannelFeatures.py
+++ Features/ChannelFeatures.py
@@ -9,7 +9,9 @@ class ChannelFeature:
     for each channel c and returns a vector of shape (numFeatures()*c,), where c is the number
     of channels provided and numFeatures() is the number of features that are calculated for
     each channel. N is the number of voxels that all belong to the same
-    supervoxel.
+    supervoxel.i
+
+    The values for each channel are normalized to 0,...,1
 
     """
 
@@ -19,10 +21,9 @@ class ChannelFeature:
     def name(self):
         return self.__class__.__name__
 
-    def numFeatures(self, channels):
+    def numFeatures(self):
         """
-        for the given number of channels, return here
-        the total number of features that will be calculated.
+        returns the number of features per channel
         """
         raise NotImplementedError()
 
@@ -45,7 +46,7 @@ class MeanChannelValueFeature(ChannelFeature):
         return np.mean(voxels, axis=0)
 
     def numFeatures(self, channels):
-        return channels
+        return 1
 
 
 ###############################
@@ -60,31 +61,47 @@ class ChannelHistogramFeature(ChannelFeature):
 
     def __init__(self, bins=10):
         self.bins = bins
+        self.binEdges = np.arange(0, 1.0, 1.0/bins)
+        self.binEdges = np.append(self.binEdges, [1.0])
 
-    def numFeatures(self, channels):
-        return  self.bins*channels
+
+
+    def numFeatures(self):
+        return  self.bins
 
     def features(self, voxels):
+        
+        nchannels = voxels.shape[1]
+        nvoxels = voxels.shape[0]
+        nbins = self.bins
 
-        raise NotImplementedError()
+        nfeatures = nchannels*self.bins
+
+        features = np.zeros((nfeatures,))
+
+        for i in range(nvoxels):
+            features[i*nbins:(i+1)*nbins] = (1.0/nvoxels)*np.histogram(voxels[i], bins=self.binEdges)[0]
+
+        
+        return features        
 
 
 # test the features
 if __name__ == "__main__":
 
     #Test data (n, c), 2 voxels and 3 channels per voxel
-    test = np.array([[1, 2, 3], [1, 2, 3]])
+    test = np.array([[0.1, 0.2, 0.9], [0.1, 0.2, 0.9]])
     
     mean = MeanChannelValueFeature()
     meanTest = mean.features(test)
-    assert(hist.numFeatures(3) == 3)
-    assert(meanTest.shape == (hist.numFeatures(1),))
+    assert(meanTest.shape[0] == 3)
 
-#    #Test ChannelHistogramFeature
-#    hist = ChannelHistogramFeature(10)
-#    histTest = hist.features(test)
-#    assert(hist.numFeatures(3) == 3*10)
-#    assert(histTest.shape == (hist.numFeatures(3),))
+
+    #Test ChannelHistogramFeature
+    hist = ChannelHistogramFeature(20)
+    histTest = hist.features(test)
+    print histTest
+    assert(histTest.shape[0] == 20*3)
     
 
 
